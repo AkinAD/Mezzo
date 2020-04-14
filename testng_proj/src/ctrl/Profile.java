18bb974f967e97c55613569e6cc8632787dd5493
alanyork <alanyork@my.yorku.ca>
2020-04-13 21:34:15 -0400

    Added profile controller, formatting cleanup on MezzoUser, added logout

--- testng_proj/src/ctrl/Profile.java
+++ testng_proj/src/ctrl/Profile.java
@@ -13,87 +13,108 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import bean.AddressBean;
 import bean.ProfileBean;
 import model.UserModel;
 
 /**
  * Servlet implementation class Profile
+ * 
+ * @author alanyork
  */
 @WebServlet({ "/Profile", "/profile" })
 public class Profile extends HttpServlet {
+	private static final String PHONE = "phone";
+	private static final String POSTAL = "postal";
+	private static final String COUNTRY = "country";
+	private static final String PROVINCE = "province";
+	private static final String STREET = "street";
+	private static final String BILL_ADDR = "BillAddr";
+	private static final String CUR_PROFILE = "CurProfile";
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Profile() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-    
-    @Override
+	private static final String PROFILE_PAGE = "profile.jsp";
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Profile() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
 	public void init(ServletConfig config) throws ServletException {
 		super.init(config);
-		//this.getServletContext().setAttribute("MS", MusicStore.getInstance());
+		// this.getServletContext().setAttribute("MS", MusicStore.getInstance());
 		this.getServletContext().setAttribute("UM", UserModel.getInstance());
 	}
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
+
 		UserModel uModel = (UserModel) this.getServletContext().getAttribute("UM");
-		Map<String, ProfileBean> data = new HashMap<String, ProfileBean>();
+		String curUsername = SessionManagement.getBoundUsername(request.getSession());
+
 		
-		if(true) // CHANGE THIS CHANGE THIS CHANGE THIS 
-		{
-			String username = SessionManagement.getBoundUsername(request.getSession());
+		String streetParam = request.getParameter(STREET);
+		String provParam = request.getParameter(PROVINCE);
+		String cuntParam = request.getParameter(COUNTRY);
+		String postParam = request.getParameter(POSTAL);
+		String phonParam = request.getParameter(PHONE);
+		
+		boolean isBillUpdateSubmission;
+		String[] upBillSubmitParams = {streetParam,provParam,cuntParam,postParam};		
+		isBillUpdateSubmission = streetParam != null && !streetParam.isEmpty();
+		for (String x : upBillSubmitParams) {
+			isBillUpdateSubmission = isBillUpdateSubmission && x != null && !x.isEmpty();
+		}
+		
+		if (isBillUpdateSubmission) {
 			try {
-				data = uModel.retrieveAccountByUsername(username);
+				uModel.updateBillingAddressByUsername(curUsername, streetParam, provParam, cuntParam, postParam, phonParam);
 			} catch (Exception e) {
 				e.printStackTrace();
-				System.out.println("FAILED TO LOGIN USER: " + uModel.getError());
 			}
 		}
-		else
-		{
-			//redirect to login
-		}
+		
+		displayPage(request, response, curUsername, uModel);
 	}
 
-	private Map<String, List<String>> populateUser(Map<String, ProfileBean> data) {
-		Map<String, List<String>> profile = new HashMap<String, List<String>>();
-		for (String stuff : data.keySet()) {
-			List<String> tmp = new ArrayList<String>();
-			tmp.add(data.get(stuff).getUserName());
-			// delete line below
-			System.out.println(data.get(stuff).getUserName());
-
-			tmp.add(data.get(stuff).getFname());
-			System.out.println(data.get(stuff).getFname());
-
-			tmp.add(data.get(stuff).getLname());
-			System.out.println(data.get(stuff).getLname());
-
-			tmp.add(data.get(stuff).getEmail());
-			System.out.println(data.get(stuff).getEmail());
-
-			tmp.add(data.get(stuff).getPrivilege());
-			System.out.println(data.get(stuff).getPrivilege());
-
-			profile.put(stuff, tmp);
-		}
-		return profile;
-	}
-	
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
+	protected void displayPage(HttpServletRequest request, HttpServletResponse response, String curUsername,
+			UserModel uModel) throws ServletException, IOException {
+		AddressBean billingAddress;
+		try {
+			billingAddress = uModel.retrieveBillingAddressByUsername(curUsername);
+		} catch (Exception e) {
+			billingAddress = new AddressBean("", "", "", "", "", "", "");
+			e.printStackTrace();
+		}
+		request.setAttribute(BILL_ADDR, billingAddress);
+
+		Map<String, ProfileBean> data = new HashMap<String, ProfileBean>();
+		try {
+			data = uModel.retrieveAccountByUsername(curUsername);
+			ProfileBean curProfile = data.values().iterator().next();
+			request.setAttribute(CUR_PROFILE, curProfile);
+			request.getRequestDispatcher(PROFILE_PAGE).forward(request, response);			
+		} catch (Exception e) {
+			response.sendError(500); // This is probably our fault
+			e.printStackTrace();
+		}		
+	}
 }
